05e1af14b313e520e644eaffe7c2b06f855d9f56
V7eGoEGO <Mrnobody@gmail.com>
2023-06-28 12:01:55 +0300

    fix bag scroll && add timerPopup act

--- js/modal.js
+++ js/modal.js
@@ -1,32 +1,45 @@
 const modalTrigger = document.querySelectorAll('[data-modal]'),
       modal = document.querySelector('.modal'),
       modalCloseBtn = document.querySelector('[data-close]');
-
-modalTrigger.forEach( btn =>{
-    console.log(btn)
-    btn.addEventListener('click', () =>{
+      modalTimerId = setTimeout(openPopUp, 3000000);
+    function openPopUp(){
+        
         modal.classList.add('show');
         modal.classList.remove('hide');
         document.body.style.overflow = 'hidden';
-    });
-});
-
-modalCloseBtn.addEventListener('click', () =>{
-    modal.classList.add('hide');
-    modal.classList.remove('show');
-    document.body.style.overflow = '';
-})
-modal.addEventListener('click', (e)=>{
-    if(e.target === modal){
-    modal.classList.add('hide');
-    modal.classList.remove('show');
-    document.body.style.overflow = '';
+        clearInterval(modalTimerId)
     }
-    document.addEventListener('keydown', (e) =>{
-        if (e.code === "Escape"){
-            modal.classList.add('hide');
-            modal.classList.remove('show');
-            document.body.style.overflow = '';
+modalTrigger.forEach( btn =>{
+
+    btn.addEventListener('click', openPopUp);
+});
+    function closerPopUp(){
+
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    modalCloseBtn.addEventListener('click', closerPopUp)
+    modal.addEventListener('click', (e)=>{
+
+        if(e.target === modal){
+            closerPopUp();
         }
-    })
+        document.addEventListener('keydown', (e) =>{
+
+            if (e.key == "Escape"){
+                closerPopUp();
+            }
+        });
+
+    function scrollOpenPopUp(){
+
+        if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openPopUp();
+            window.removeEventListener('scroll', scrollOpenPopUp);
+        }
+    }
+    window.addEventListener('scroll', scrollOpenPopUp)
+
 })

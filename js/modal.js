127c43abc52889d486bb44d22a5b2bceec3af532
V7eGoEGO <Mrnobody@gmail.com>
2023-06-30 13:59:22 +0300

    "form post && new popUp"

--- js/modal.js
+++ js/modal.js
@@ -1,46 +1,122 @@
-const modalTrigger = document.querySelectorAll('[data-modal]'),
-      modal = document.querySelector('.modal'),
-      modalCloseBtn = document.querySelector('[data-close]');
-      modalTimerId = setTimeout(openPopUp, 3000000);
-    function openPopUp(){
-        
-        modal.classList.add('show');
-        modal.classList.remove('hide');
-        document.body.style.overflow = 'hidden';
-        clearInterval(modalTimerId)
-    }
-modalTrigger.forEach( btn =>{
+document.addEventListener('DOMContentLoaded', ()=>{
+	const modalTrigger = document.querySelectorAll("[data-modal]"),
+		  modal = document.querySelector(".modal"),
+		  modalTimerId = setTimeout(openPopUp, 3000000);
+	function openPopUp() {
+		modal.classList.add("show");
+		modal.classList.remove("hide");
+		document.body.style.overflow = "hidden";
+		clearInterval(modalTimerId);
+	}
+	modalTrigger.forEach((btn) => {
+		btn.addEventListener("click", openPopUp);
+	});
+	function closerPopUp() {
+		modal.classList.add("hide");
+		modal.classList.remove("show");
+		document.body.style.overflow = "";
+	}
 
-    btn.addEventListener('click', openPopUp);
-});
-    function closerPopUp(){
+	
+	modal.addEventListener("click", (e) => {
+		if (e.target === modal || e.target.getAttribute('data-close') == '') {
+			closerPopUp();
+		}
+	document.addEventListener("keydown", (e) => {
+		if (e.key === "Escape") {
+			closerPopUp();
+		}
+	});
+	// Активация Скролла
+	// function scrollOpenPopUp() {
+	// 	if (
+	// 		window.scrollY + document.documentElement.clientHeight >=
+	// 		document.documentElement.scrollHeight
+	// 	) {
+	// 		openPopUp();
+	// 		window.removeEventListener("scroll", scrollOpenPopUp);
+	// 	}
+	//   }
+	// window.addEventListener("scroll", scrollOpenPopUp);
+	});
 
-        modal.classList.add('hide');
-        modal.classList.remove('show');
-        document.body.style.overflow = '';
-    }
+	//  Работа с отпракой
+	const forms = document.querySelectorAll('form');
+	const message = {
+		loading: 'img/form/spinner.svg',
+		success: 'Спасибо! Скоро мы с вами свяжемся',
+		failure: 'Что-то пошло не так...'
+	};
 
-    modalCloseBtn.addEventListener('click', closerPopUp)
-    modal.addEventListener('click', (e)=>{
+	forms.forEach(item => {
+		postData(item);
+	});
+	function postData(form) {
+		form.addEventListener('submit', (e) => {
+			e.preventDefault();
 
-        if(e.target === modal){
-            closerPopUp();
-        }
-        document.addEventListener('keydown', (e) =>{
+			let statusMessage = document.createElement('img');
+			statusMessage.src = message.loading;
+			statusMessage.style.cssText = `
+				display: block;
+				margin: 0 auto;
+			`;
+			form.insertAdjacentElement('afterend', statusMessage);
+		
+			const request = new XMLHttpRequest();
+			request.open('POST', 'server.php');
+			request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+			const formData = new FormData(form);
 
-            if (e.key === "Escape"){
-                closerPopUp();
-            }
-        });
+			const object = {};
+			formData.forEach(function(value, key){
+				object[key] = value;
+			});
+			const json = JSON.stringify(object);
 
-    function scrollOpenPopUp(){
+			request.send(json);
 
-        if (window.scrollY + document.documentElement.clientHeight >= 
-            document.documentElement.scrollHeight) {
-            openPopUp();
-            window.removeEventListener('scroll', scrollOpenPopUp);
-        }
-    }
-    window.addEventListener('scroll', scrollOpenPopUp)
+
+			request.addEventListener('load', () => {
+				if (request.status === 200) {
+					swodModalPost(message.success);
+					statusMessage.remove();
+					form.reset();
+				} else {
+					console.log(request.failure);
+					swodModalPost(message.failure);
+				}
+			});
+		});
+	}
+
+
+
+	function swodModalPost(massage){
+		const prevMiodalDialog = document.querySelector('.modal__dialog');
+
+		prevMiodalDialog.classList.add('hide');
+		openPopUp();
+
+		const createNewModal = document.createElement('div');
+		createNewModal.classList.add('modal__dialog');
+		createNewModal.innerHTML =`
+			<div class = "modal__content"> 
+				<div class = "modal__close" data close>x</div>
+				<div class = "modal__title">${massage}</div>
+			</div>
+		`;
+		console.log(createNewModal)
+		document.querySelector('.modal').append(createNewModal);
+		console.log(document.querySelector('.modal'))
+		setTimeout(()=>{
+			createNewModal.remove();
+			prevMiodalDialog.classList.add('show');
+			prevMiodalDialog.classList.remove('hide');
+			closerPopUp();
+		},4000)
+
+		document.querySelector('.modal').append(createNewModal);
+	}
 
 })
